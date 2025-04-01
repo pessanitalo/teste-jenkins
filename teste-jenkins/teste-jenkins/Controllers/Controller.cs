62d9f77997eef6532ce64cb7f0ef138fc010c0ec
italo pessan <92962625+pessanitalo@users.noreply.github.com>
2025-04-01 11:40:35 -0300

    teste

---
+++ teste-jenkins/teste-jenkins/Controllers/Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace teste_jenkins.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult teste()
+        {
+            return Ok("OK");
+        }
+    }
+}
